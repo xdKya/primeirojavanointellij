18bee964a4753dc9f048b57a716c19d860304b94
xdKya <amandaetiel@gmail.com>
2022-07-22 19:13:49 -0300

    criando class Livro

--- src/br/com/dio/primeiroprograma.java
+++ src/br/com/dio/primeiroprograma.java
@@ -13,6 +13,9 @@ public class primeiroprograma {
 
         System.out.println(gatineo);
 
+        Livro livro1 = new Livro("O mistério de Belladona", 500);
+        System.out.println(livro1);
+
 
        /* int a = 5;
         int b = 8;
@@ -23,3 +26,36 @@ public class primeiroprograma {
     }
 }
 
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+}
